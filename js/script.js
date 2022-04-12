d687f0274a990b24622129296842d08651ad90bb
Eoty <anderyenfresa@gmail.com>
2022-04-12 17:40:02 +0100

    second exercise

--- js/script.js
+++ js/script.js
@@ -10,8 +10,18 @@ let personalMovieDb = {
     privat: false
 };
 
-personalMovieDb.movies[prompt("last watched film?", "")] = prompt("how u gonna score it?", "0");
+const a = prompt("last watched film?", ""),
+      b = prompt("how u gonna score it?", "0");
 
+
+for(let i = 0; i<3; i++) {
+    personalMovieDb.movies[a] = b;
+
+    if (a == '') continue; else if (a.length> 50) continue;
+
+}
+
+(personalMovieDb.count<10) ? alert("you'are poor as fuck") : (personalMovieDb<30) ? alert('That"s about alright') : (personalMovieDb>29) ? alert("Bruh that's too much") : alert("You are fuking chicher");
 
 personalMovieDb.movies[prompt("lasts watched film?", "")] = prompt("how us gonna score it?", "0");
 
