4f1eb8e8932ae81b9c1bd0ceaca70b5a2e303bb8
Eoty <anderyenfresa@gmail.com>
2022-04-12 12:07:31 +0100

    made exercise

--- js/script.js
+++ js/script.js
@@ -1,47 +1,19 @@
 "use strict";
-let a = 4;
-let some = ['n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'];
+
+let numberOfFilms = prompt("how many films have you already watched?", "0");
+
+let personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDb.movies[prompt("last watched film?", "")] = prompt("how u gonna score it?", "0");
 
 
-
-function p(num) {
-    if(num<=1) return false;
-    let val = Math.sqrt(num);
-
-    let ar = Array(val).fill().map((x, i) => i);
-    ar.shift();
-    ar.shift();
-    let ar2 =ar.filter(n => n % 2 !== 0),
-    ar3 =ar2.filter(n => n % 3 !== 0),
-    ar4 = ar3.filter(n => n % 5 !== 0),
-    ar5 = ar4.filter(n => n % 7 !== 0),
-    ar6 = ar5.filter(n => n % 11 !== 0);
-
-}
-
-function s(num) {
-    if (num <= 1) return false;
-    if (num == 2) return true;
-
-    let val = Math.sqrt(num);
-    let ar = Array(Math.round(val)).fill().map((x, i) => i);
-    ar.shift();
-    ar.shift();
-    if(num%2==0) return false;
-    let ar2 =ar.filter(n => n % 2 !== 0);
-    if(num%3==0) return false;
-    let ar3 =ar2.filter(n => n % 3 !== 0);
-    if(num%5==0) return false;
-    let ar4 = ar3.filter(n => n % 5 !== 0);
-    if(num%7==0) return false;
-    let ar5 = ar4.filter(n => n % 7 !== 0);
-    if(num%11==0) return false;
-    let ar6 = ar5.filter(n => n % 11 !== 0);
-    console.log(ar6);
-    for (let i of ar) {
-        if (num%i ==0) return false;
-    } return true;
-}
+personalMovieDb.movies[prompt("lasts watched film?", "")] = prompt("how us gonna score it?", "0");
 
 
-console.log(s(344213809));
+console.log(personalMovieDb);
